a2c9e2a3c017200aaae76c0b7615cad0724bc30e
Jon Christie <jonpchristie@gmail.com>
2020-09-03 21:51:06 -0400

    bg

--- dist/main.js
+++ dist/main.js
@@ -18294,7 +18294,7 @@ function () {
     this.move = this.move.bind(this);
     this.jumpTime = 0;
     this.keydown = false;
-    this.sprite = new _sprites__WEBPACK_IMPORTED_MODULE_0__["default"]("https://github.com/taeinha/js-climber/blob/master/src/images/DinoSprites.png?raw=true", "https://github.com/taeinha/js-climber/blob/master/src/images/DinoSpritesLeft.png?raw=true", 576, 24, 1, 24);
+    this.sprite = new _sprites__WEBPACK_IMPORTED_MODULE_0__["default"]("https://raw.githubusercontent.com/mathcodes/Catwalk2DPlatform/master/src/images/catpounceR.png", "https://raw.githubusercontent.com/mathcodes/Catwalk2DPlatform/master/src/images/catpounceL.png", 576, 24, 1, 24);
   }
 
   _createClass(Climber, [{
